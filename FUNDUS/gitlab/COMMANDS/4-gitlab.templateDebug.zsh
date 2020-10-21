d06b6314c23620c3eb0a4d7b5d62e2bf64e11d74
Frank Beutelschieß <frank@thesym.site>
2020-10-21 16:51:42 +0200

    global: add gitlab to FUNDUS

---
+++ FUNDUS/gitlab/COMMANDS/4-gitlab.templateDebug.zsh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+###
+##
+# * DEBUG
+##
+###
+helm template \
+	--namespace harbor \
+	harbor ../CHART/harbor \
+	--debug \
+        -s templates/ingress/ingress.yaml
+        #-s templates/synapse/configmap.yaml
