06d3bf3485d297876949448504d0b70ec518712a
Frank Beutelschieß <frank@thesym.site>
2020-10-26 01:59:02 +0100

    dashboard: initial re-commit (Task.yaml + persistence)

--- STRUCTURAL/dashboard/helm/COMMANDS/10-dashboard.chartRemove.zsh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zsh
-###
-##
-# * REMOVAL
-##
-###
-helm uninstall kubernetes-dashboard
-kubectl delete namespace kubernetes-dashboard
--- STRUCTURAL/dashboard/yamls/dashboard.delete.zsh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-
-kubectl delete namespace kubernetes-dashboard
-#kubectl --namespace dev-nginx delete ingress nginx 
-#kubectl --namespace dev-nginx delete services   nginx 
-#kubectl --namespace dev-nginx delete deployment nginx 
