464eceb478c06d6fa5d63eb1965b098f133001e9
Frank Beutelschieß <frank@thesym.site>
2020-12-01 05:40:30 +0100

    powerdns: implement TDD for DNS

--- APPS/powerDNS/test/dnsTest.zsh
+++ APPS/powerDNS/test/dnsTest.zsh
@@ -1,37 +1,59 @@
 #!/usr/bin/env zsh
-#
-#
 ###
 ## * compare ns1.domain.com  nsOnNuc
 #
+#SIDE_BY_SIDE="-y"
+SIDE_BY_SIDE=""
+
+###
+## ** *.thesym.site ANY
+#
+echo -e "\n\n\e[1;34m*.thesym.site \e[33mANY\e[0m\n"
+colordiff ${SIDE_BY_SIDE} \
+  <(dig @ns1.domain.com +noall +answer +multiline '*.thesym.site' ANY | sort -k4) \
+  <(dig @${KUBE_HOST_EXTERNAL_IP} +noall +answer +multiline '*.thesym.site' ANY | sort -k4)
+
+
+###
+## ** mail.thesym.site ANY
+#
+echo -e "\n\n\e[1;34mmail.thesym.site \e[33mANY\e[0m\n"
+colordiff ${SIDE_BY_SIDE} \
+  <(dig @ns1.domain.com +noall +answer +multiline 'mail.thesym.site' ANY | sort -k4) \
+  <(dig @${KUBE_HOST_EXTERNAL_IP} +noall +answer +multiline 'mail.thesym.site' ANY | sort -k4)
+
+
+###
+## ** _dmarc.thesym.site TXT
+#
+echo -e "\n\n\e[1;34m_dmarc.thesym.site \e[33mTXT\e[0m\n"
+colordiff ${SIDE_BY_SIDE} \
+  <(dig @ns1.domain.com +noall +answer +multiline _dmarc.thesym.site TXT | sort -k4) \
+  <(dig @${KUBE_HOST_EXTERNAL_IP} +noall +answer +multiline _dmarc.thesym.site TXT | sort -k4)
+
+
+###
+## ** dkim._domainkey.thesym.site TXT
+#
+echo -e "\n\n\e[1;34mdkim._domainkey.thesym.site \e[33mTXT\e[0m\n"
+colordiff ${SIDE_BY_SIDE} \
+  <(dig @ns1.domain.com +noall +answer +multiline dkim._domainkey.thesym.site TXT | sort -k4) \
+  <(dig @${KUBE_HOST_EXTERNAL_IP} +noall +answer +multiline dkim._domainkey.thesym.site TXT | sort -k4)
+
+
+###
+## ** thesym.site PTR
+#
+echo -e "\n\n\e[1;34mthesym.site \e[33mPTR\e[0m\n"
+colordiff -b ${SIDE_BY_SIDE} \
+  <(echo ${KUBE_HOST_EXTERNAL_IP} | awk -F . '{print $4"."$3"."$2"."$1".in-addr.arpa. 1800 IN PTR thesym.site."}') \
+  <(dig @${KUBE_HOST_EXTERNAL_IP}  +noall +answer -x ${KUBE_HOST_EXTERNAL_IP})
+
+
 ###
 ## ** thesym.site ANY
 #
 echo -e "\n\n\e[1;34mthesym.site \e[33mANY\e[0m\n"
-colordiff -y \
-	<(dig @${KUBE_HOST_EXTERNAL_IP} +noall +answer  thesym.site ANY | sort -k4) \
-	<(dig @ns1.domain.com +noall +answer  thesym.site ANY | sort -k4)
-
-
-echo -e "\n\n\e[1;34m*.thesym.site \e[33mANY\e[0m\n"
-colordiff -y \
-	<(dig @${KUBE_HOST_EXTERNAL_IP} +noall +answer +multiline '*.thesym.site' ANY | sort -k4) \
-	<(dig @ns1.domain.com +noall +answer +multiline '*.thesym.site' ANY | sort -k4)
-
-
-echo -e "\n\n\e[1;34mmail.thesym.site \e[33mANY\e[0m\n"
-colordiff -y \
-	<(dig @${KUBE_HOST_EXTERNAL_IP} +noall +answer +multiline 'mail.thesym.site' ANY | sort -k4) \
-	<(dig @ns1.domain.com +noall +answer +multiline 'mail.thesym.site' ANY | sort -k4)
-
-
-echo -e "\n\n\e[1;34m_dmarc.thesym.site \e[33mTXT\e[0m\n"
-colordiff -y \
-	<(dig @${KUBE_HOST_EXTERNAL_IP} +noall +answer +multiline _dmarc.thesym.site TXT | sort -k4) \
-	<(dig @ns1.domain.com +noall +answer +multiline _dmarc.thesym.site TXT | sort -k4)
-
-
-echo -e "\n\n\e[1;34mdkim._domainkey.thesym.site \e[33mTXT\e[0m\n"
-colordiff -y \
-	<(dig @${KUBE_HOST_EXTERNAL_IP} +noall +answer +multiline dkim._domainkey.thesym.site TXT | sort -k4) \
-	<(dig @ns1.domain.com +noall +answer +multiline dkim._domainkey.thesym.site TXT | sort -k4)
+colordiff ${SIDE_BY_SIDE} \
+          <(dig @ns1.domain.com +noall +answer  thesym.site ANY | sort -k4) \
+          <(dig @${KUBE_HOST_EXTERNAL_IP} +noall +answer  thesym.site ANY | sort -k4)
---
+++ APPS/powerDNS/test/testLoop.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+#
+local BASE_DIR=$(dirname $0:h:P)
+while true; do
+  read "?Press ENTER to start the test"
+  ${BASE_DIR}/test/dnsTest.zsh
+done
